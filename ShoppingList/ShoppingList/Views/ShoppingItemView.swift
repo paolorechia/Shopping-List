06922f3fb9d470f345e28e03f62a8c93661bfd2a
Paolo Rechia <paolorechia@gmail.com>
2021-01-09 16:48:24 -0300

    CF2-0 Move CoreData Files back to Root

--- ShoppingList/ShoppingList/Views/ShoppingItemView.swift
+++ ShoppingList/ShoppingList/Views/ShoppingItemView.swift
@@ -8,8 +8,60 @@
 import SwiftUI
 
 struct ShoppingItemView: View {
+    @State var itemName: String = "";
+    @State var itemPriceUnit: String = "0";
+    @State var itemPriceCents: String = "0";
+    @State var itemQuantity: String = "0";
+    @State var itemBrand: String = "";
+    
     var body: some View {
-        Text("Hello, Shopping Item")
+        VStack {
+            Image(systemName: "bag")
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                .font(.system(size: 60))
+                .padding()
+            HStack {
+                Text("Name")
+                    .foregroundColor(Color.blue)
+                    .frame(width: 150)
+                TextField("name", text: $itemName)
+                    .frame(width: 150)
+            }
+            HStack {
+                Text("Name")
+                    .frame(width: 150)
+                    .foregroundColor(Color.blue)
+                TextField("name", text: $itemName)
+                    .frame(width: 150)
+            }
+            HStack {
+                Text("Quantity")
+                    .frame(width: 150)
+                    .foregroundColor(Color.blue)
+                TextField("quantity", text: $itemQuantity)
+                    .frame(width: 150)
+            }
+            HStack {
+                Text("Brand")
+                    .frame(width: 150)
+                    .foregroundColor(Color.blue)
+
+                TextField("brand", text: $itemName)
+                    .frame(width: 150)
+
+            }
+            .padding()
+            Button(action: saveNewItem) {
+                Text("Save")
+                    .frame(width: 300, height: 50)
+                    .border(Color.blue, width: 1)
+            }
+        }
+        .padding()
+        .navigationTitle("Add Item")
+    }
+    private func saveNewItem() {
+        print("Save item!")
     }
 }
 
