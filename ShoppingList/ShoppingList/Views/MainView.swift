34b7feaa8d5f5e97beae842c10ca5bdebd3d0535
Paolo Rechia <paolorechia@gmail.com>
2021-01-09 15:56:26 -0300

    CPT-1 Main Menu UI

--- ShoppingList/ShoppingList/Views/MainView.swift
+++ ShoppingList/ShoppingList/Views/MainView.swift
@@ -11,7 +11,34 @@ struct MainView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
     var body: some View {
-        Text("Main Menu")
+        VStack() {
+            Image(systemName: "cart")
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                .font(.system(size: 120))
+            Button(action: startShopping) {
+                Text("New Shopping")
+            }
+            .frame(width: 200, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            .border(Color.blue, width: 1)
+            .padding()
+
+            
+            Button(action: viewHistory) {
+                Text("History")
+            }
+            .frame(width: 200, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            .border(Color.blue, width: 1)
+
+
+        }
+    }
+    
+    func startShopping() {
+        print("Start Shopping!")
+    }
+    
+    func viewHistory() {
+        print("View History")
     }
 }
 
